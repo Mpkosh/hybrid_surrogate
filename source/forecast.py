4dbb765420499226dbc593515e8746ecb3e82358
Андрей Корзин <andrey@Andrejs-MacBook-Air.local>
2025-09-19 17:43:30 +0300

    .

--- source/forecast.py
+++ source/forecast.py
@@ -1,5 +1,6 @@
 import pymc as pm
 import numpy as np
+import arviz as az
 from source.model_output import SEIRModelOutput, SEIRParams
 from source.SEIR_network import SEIRNetworkModel
 
@@ -8,11 +9,7 @@ class SEIRForecaster():
     def __init__(self, initial_params: SEIRParams):
         self.best_params = initial_params
 
-    def calibrate(self, model, observed):
-        epsilon = 2000
-        sample = 100
-
-        print(np.where(observed == 0))
+    def calibrate(self, model, observed, epsilon=2000):
         epidemic_len = len(observed)
         zeros_arr = np.where(observed == 0)[0]
         if len(zeros_arr) > 1:
@@ -38,9 +35,19 @@ class SEIRForecaster():
             )
             idata = pm.sample_smc(progressbar=True, draws=5000)
 
+        # az.plot_pair(
+        #     idata,
+        #     var_names=["beta", "alpha"],
+        #     kind=["scatter", "kde"],
+        #     kde_kwargs={"fill_last": False},
+        #     marginals=True,
+        #     point_estimate="mode",
+        #     figsize=(11.5, 5),
+        # )
         posterior = idata.posterior.stack(samples=("draw", "chain"))
         alpha_arr = posterior["alpha"].values
         beta_arr = posterior["beta"].values
         self.best_params.alpha = alpha_arr.mean()
         self.best_params.beta = beta_arr.mean()
-        return {'alpha_arr': alpha_arr, 'beta_arr': beta_arr}
+        return idata
+        # return {'alpha_arr': alpha_arr, 'beta_arr': beta_arr}
