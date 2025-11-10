a2c1475ece86f1ab2687ad39bade377dfef8abc6
Mpkosh <61066498+Mpkosh@users.noreply.github.com>
2025-11-10 18:03:56 +0300

    .

--- source/SEIR_network.py
+++ source/SEIR_network.py
@@ -9,33 +9,17 @@ from .model_output import SEIRModelOutput, SEIRParams
 
 
 class SEIRNetworkModel():
-    def __init__(self, population: int, ntype: str,
-                chosen_seed):
+    def __init__(self, population: int):
         self.population = population
 
         # FOLLOWING PARAMETERS ARE EPIDEMICALLY DETERMINED
         # R_0 HERE LIES IN RANGE [1; 2.5]
         self.min_params = SEIRParams(
-
-            beta=1/9, gamma=1/5, delta=1/9, 
-            init_inf_frac=1e-6, init_rec_frac=1e-2)
+            alpha=0.99, beta=1/9, gamma=1/5, delta=1/9, init_inf_frac=1e-6)
         self.max_params = SEIRParams(
-            beta=0.625, gamma=1, delta=1/4, 
-            init_inf_frac=1e-3, init_rec_frac=2e-1)
+            alpha=0.8, beta=0.625, gamma=1, delta=1/4, init_inf_frac=1e-3)
         self.last_sim_params = None
-        
-        if ntype=='ba':
-            self.G = nx.barabasi_albert_graph(population, 5, seed=chosen_seed)
-            print('ba')
-        elif ntype=='sw':
-            self.G = nx.watts_strogatz_graph(population, 5, 0.1, seed=chosen_seed)
-            print('sw')
-        elif ntype=='r':
-            # чтобы средняя степень была 8
-            self.G = nx.fast_gnp_random_graph(population, 5/population, seed=chosen_seed)
-            print('r')
-        
-
+        self.G = nx.barabasi_albert_graph(population, m=5, seed=42)
 
     @staticmethod
     def find_nearest_idx(array, value):
@@ -43,29 +27,17 @@ class SEIRNetworkModel():
         idx = (np.abs(array - value)).argmin()
         return idx
 
-    
-    def transform_event_times_to_days(self, model_output, tmax,
-                                      I_frac_switch):
+    def transform_event_times_to_days(self, model_output, tmax):
         indices = []
         for day in range(tmax):
             index = self.find_nearest_idx(model_output.t, day)
             indices.append(index)
-            if model_output.I[index] > self.population*I_frac_switch:
-                print(day)
-                break
-            
-        new_model_output = SEIRModelOutput(model_output.t[indices], 
-                                           model_output.S[indices],
-                                           model_output.E[indices], 
-                                           model_output.I[indices],
+        new_model_output = SEIRModelOutput(model_output.t[indices], model_output.S[indices],
+                                           model_output.E[indices], model_output.I[indices],
                                            model_output.R[indices])
         return new_model_output
 
-
-    def simulate(self, beta=1/7*1.5, gamma=1/2, delta=1/7, 
-                 init_inf_frac=1e-4, init_rec_frac=0.15, 
-                 tmax: int = 150, I_frac_switch=0.1):
-
+    def simulate(self, alpha=0.85, beta=1/7*1.5, gamma=1/2, delta=1/7, init_inf_frac=1e-4, tmax: int = 150):
         '''
         Parameters:
 
@@ -89,15 +61,7 @@ class SEIRNetworkModel():
             "Incorrect initial conditions, immune + infected > population size!"
         for node in range(initial_recovered):
             initial_status[node+initial_infected] = 'R'
-            
-        seir_o = SEIRModelOutput(*EoN.Gillespie_simple_contagion(
-                                        self.G, H, J, initial_status,
-                                        return_statuses=('S', 'E', 
-                                                         'I', 'R'),
-                                        tmax=tmax,
-                                        I_frac_switch=I_frac_switch)
-                                 )
-        self.result = self.transform_event_times_to_days(seir_o, tmax,
-                                                         I_frac_switch)
-        
+        self.result = self.transform_event_times_to_days(SEIRModelOutput
+                                                         (*EoN.Gillespie_simple_contagion(self.G, H, J, initial_status,
+                                                                                          return_statuses=('S', 'E', 'I', 'R'), tmax=tmax)), tmax)
         return self.result
